5d76096df3b361aa1f950bd8c18be74359e6a2d5
Ignas Mikalajunas <ignas@uber.com>
2014-05-09 20:14:43 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ requires = [
 
 
 setup(name='nose_xunitmp',
-      version='0.3',
+      version='0.3.1',
       description='Xunit output when running multiprocess tests using nose',
       long_description=README + '\n\n' +  CHANGES,
       classifiers=[
